9d081dddc251754153c32a51a691e25e4af48ba9
sameerul-hak <sameerulhakofficial@gmailcom>
2024-03-22 10:04:02 +0530

    hello

---
+++ qrcode-generator-reactJS/src/Config.js
@@ -0,0 +1,10 @@
+
+// this url for backend 
+// once dockerized update the backend container url here  like http://localhost:5000
+const url="https://brainy-dirndl-hen.cyclic.app";
+
+
+
+//change the this url once got public ip to access the website
+const qrcodeurl="http://ip:port" // http://193.34.175.32:9090
+export {url,qrcodeurl};
